51c4c7c475233a841d808f95c9d99f02682b80f3
fankaka8 <itphongtran@gmail.com>
2018-05-17 16:18:34 +0700

    Install react-native-maps

--- android/app/src/main/java/com/shop/MainApplication.java
+++ android/app/src/main/java/com/shop/MainApplication.java
@@ -22,9 +22,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+        new MainReactPackage(),
+        new MapsPackage()
       );
-    }
+     }
 
     @Override
     protected String getJSMainModuleName() {
