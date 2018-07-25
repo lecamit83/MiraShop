59b222c8fe1f5ddf0c41b145e626db10cd7f1302
Le Cam <thantoandoc83@gmail.com>
2018-07-25 14:54:31 +0700

    Details

--- src/components/Details.js
+++ src/components/Details.js
@@ -38,7 +38,6 @@ import {
   Toast
 } from "native-base";
 
-
 // create a component
 class Details extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -46,31 +45,503 @@ class Details extends Component {
   });
   constructor(props) {
     super(props);
-    this.state = {
-      arrImage: [
-        require("../images/1.png"),
-        require("../images/2.png"),
-        require("../images/3.png")
-      ]
-    };
   }
 
   render() {
-    const {
-      container,
-      frameImage,
-      line,
-      intro,
-      wrap,
-      textStyle,
-      wrapImage,
-      icon
-    } = styles;
+    const { icon, line } = styles;
 
     const { items, isProfile } = this.props.navigation.state.params;
     const { navigation } = this.props;
-    console.log(this.props);
-    const INFOR = ( <Card>
+    console.log(items);
+    const INFOR = [
+      <Card>
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Thành Phần
+          </Text>
+        </CardItem>
+        <CardItem bordered>
+          <Body>
+            <Text>{items.thuoc_thanhphan}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Thông tin mô tả
+          </Text>
+        </CardItem>
+        <CardItem bordered>
+          <Body>
+            <Text>{items.product_thongtinsanpham}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Nhóm dược lý
+          </Text>
+        </CardItem>
+        <CardItem bordered>
+          <Body>
+            <Text>{items.thuoc_nhomduocly}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Chỉ định
+          </Text>
+        </CardItem>
+        <CardItem bordered>
+          <Body>
+            <Text>{items.thuoc_chidinh}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Liều Lượng & Cách Dùng
+          </Text>
+        </CardItem>
+        <CardItem bordered>
+          <Body>
+            <Text>{items.thuoc_lieuluongcachdung}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Tác dụng
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.thuoc_tacdung}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Dược lực học
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.thuoc_duocluchoc}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Chống chỉ định
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.thuoc_chongchidinh}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Thận trọng
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.thuoc_thantrong}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Dạng bào chế
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.thuoc_dangbaoche}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Đóng gói
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.thuoc_quycachdonggoi}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Bảo quản
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.thuoc_dieukienbaoquan}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Nhà sản xuất
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.product_nhasanxuat}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Xuất xứ
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.product_xuatxu}</Text>
+          </Body>
+        </CardItem>
+      </Card>,
+      <Card>
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Thành Phần
+          </Text>
+        </CardItem>
+        <CardItem bordered>
+          <Body>
+            <Text>{items.thuoc_thanhphan}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Thông tin mô tả
+          </Text>
+        </CardItem>
+        <CardItem bordered>
+          <Body>
+            <Text>{items.product_thongtinsanpham}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Liều Lượng & Cách Dùng
+          </Text>
+        </CardItem>
+        <CardItem bordered>
+          <Body>
+            <Text>{items.tpcn_lieuluongcachdung}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Tác dụng
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.tpcn_congdung}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Đối tượng sử dụng
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.tpcn_doituongsudung}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Số đăng ký quảng cáo
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.tpcn_dkqc}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Tác dụng phụ
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.tpcn_tacdungphu}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Thận trọng
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.tpcn_thantrong}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Quá liều & xử trí
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.tpcn_qualieuxutri}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Dạng bào chế
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.tpcn_dangbaoche}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Đóng gói
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.tpcn_quycachdonggoi}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Bảo quản
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.tpcn_dieukienbaoquan}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Nhà sản xuất
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.product_nhasanxuat}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Xuất xứ
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.product_xuatxu}</Text>
+          </Body>
+        </CardItem>
+      </Card>,
+      <Card>
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Thành Phần
+          </Text>
+        </CardItem>
+        <CardItem bordered>
+          <Body>
+            <Text>{items.vttb_thanhphan}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Thông tin mô tả
+          </Text>
+        </CardItem>
+        <CardItem bordered>
+          <Body>
+            <Text>{items.product_thongtinsanpham}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+          Thông số kĩ thuật
+          </Text>
+        </CardItem>
+        <CardItem bordered>
+          <Body>
+            <Text>{items.vttb_thongsokithuat}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Chỉ định
+          </Text>
+        </CardItem>
+        <CardItem bordered>
+          <Body>
+            <Text>{items.vttb_chidinh}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Hướng dẫn sử dụng
+          </Text>
+        </CardItem>
+        <CardItem bordered>
+          <Body>
+            <Text>{items.vttb_huongdansudung}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Tác dụng
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.vttb_tacdung}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Model
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.vttb_model}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Nhãn hiệu
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.vttb_nhanhieu}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Phụ kiện
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.vttb_phukien}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Thông tin khác
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.vttb_thongtinkhac}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Chú ý
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.vttb_chuy}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Kích thước
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.vttb_kichthuoc}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Trọng lượng
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.vttb_trongluong}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Bảo quản
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.vttb_baoquan}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Nhà sản xuất
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.product_nhasanxuat}</Text>
+          </Body>
+        </CardItem>
+        <View style={line} />
+        <CardItem header>
+          <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+            Xuất xứ
+          </Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{items.product_xuatxu}</Text>
+          </Body>
+        </CardItem>
+      </Card>,
+      <Card>
       <CardItem header>
         <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
           Thành Phần
@@ -78,58 +549,56 @@ class Details extends Component {
       </CardItem>
       <CardItem bordered>
         <Body>
-          <Text>
-            Mô tả về thành phần của thuốc hoặc sản phẩm. Tại đây có thể là
-            thông tin khác dựa vào dữ liệu được nhập từ admin hoặc từ nhà
-            cung cấp.
-          </Text>
+          <Text>{items.mypham_thanhphan}</Text>
         </Body>
       </CardItem>
+      <View style={line} />
       <CardItem header>
         <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
-          Thông tin mô tả
+        Công dụng
         </Text>
       </CardItem>
       <CardItem bordered>
         <Body>
-          <Text>
-            Immersive Immersive mode is best when users need to interact
-            heavily with the screen, such as playing a game or interacting
-            with an image gallery. You may show and hide your app’s controls
-            along with the system bars as needed. Interaction: Swipe from
-            any edge of the screen to make the system bars appear. The first
-            time an app goes full-screen, this swipe gesture is explained.
-            Edge swipe exception: Apps that use the swipe-from-edge gesture
-            to perform actions should also display the system bars when that
-            gesture is used. For example, a drawing app that uses an edge
-            swipe (such as to draw a line) should also display the system
-            bars semi-transparently for a few seconds any time that gesture
-            occurs.
-          </Text>
+          <Text>{items.mypham_congdung}</Text>
         </Body>
       </CardItem>
+      <View style={line} />
       <CardItem header>
         <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
-          Tác dụng
+        Thành phần
+        </Text>
+      </CardItem>
+      <CardItem bordered>
+        <Body>
+          <Text>{items.mypham_thanhphan}</Text>
+        </Body>
+      </CardItem>
+      <View style={line} />
+      <CardItem header>
+        <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
+          Nhà sản xuất
         </Text>
       </CardItem>
       <CardItem>
         <Body>
-          <Text>
-            Mô tả về tác dụng của thuốc hoặc sản phẩm. Tại đây có thể là
-            thông tin khác dựa vào dữ liệu được nhập từ admin hoặc từ nhà
-            cung cấp.
-          </Text>
+          <Text>{items.product_nhasanxuat}</Text>
         </Body>
       </CardItem>
-    </Card>);
-    const SSS = (
+      <View style={line} />
       <CardItem header>
         <Text style={{ fontSize: 17, color: BACKGROUND_COLOR_HEADER }}>
-        {items.ten}
+          Xuất xứ
         </Text>
       </CardItem>
-    );
+      <CardItem>
+        <Body>
+          <Text>{items.product_xuatxu}</Text>
+        </Body>
+      </CardItem>
+    </Card>
+    ];
+
     return (
       <ScrollView
         style={{
@@ -227,7 +696,7 @@ class Details extends Component {
           <CardItem bordered>
             <Body>
               <Image
-                source={{uri: items.product_image}}
+                source={{ uri: items.product_image }}
                 style={{ height: 200, width: width - 40, flex: 1 }}
               />
             </Body>
@@ -237,9 +706,9 @@ class Details extends Component {
           >
             <Left>
               <Text
-                style={{ color: "#FFFFFF", fontWeight: "bold", fontSize: 20 }}
+                style={{ fontWeight: "bold", fontSize: 17, color: "#FFFFFF" }}
               >
-                1.500.000 đ
+                Hạn sử dụng: {items.product_hansudung}
               </Text>
             </Left>
             <Right>
@@ -268,7 +737,7 @@ class Details extends Component {
             </Right>
           </CardItem>
         </Card>
-        {INFOR}
+        {INFOR[items.product_type - 1]}
       </ScrollView>
     );
   }
@@ -285,7 +754,7 @@ const styles = StyleSheet.create({
 
   frameImage: {
     width: width - 6,
-    height: height * 4 / 10,
+    height: (height * 4) / 10,
     marginTop: 2
   },
 
